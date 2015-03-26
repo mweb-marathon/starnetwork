1496ce72259ccad9dbb96fa3da0d088f8ff98232
Maxim Bogdanov <sin666m4a1fox@gmail.com>
2015-03-26 15:45:05 +0300

    SCHNEP-236

--- wp-content/themes/StarNetwork/page-star-speak.php
+++ wp-content/themes/StarNetwork/page-star-speak.php
@@ -16,132 +16,134 @@ $url = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));
 <?php get_header('star_network'); ?>
     <div class="star-main-content-wrapper" style="background: url('<?php echo $url; ?>') no-repeat; min-height: 600px;">
         <div id="content" class="row widecolumn extra-pages">
-            <div class="star-network-content">
-                <?php if (have_posts()) : ?>
-                    <?php while (have_posts()) : the_post(); ?>
-                        <div class="star-network-additional-page-title">
-                            <?php echo get_the_title(); ?>
-                        </div>
-
-                        <div class="star-network-additional-page-sublime">
-                            <?php echo get_the_excerpt(); ?>
-                        </div>
-
-                        <?php if (get_the_content() !== ''): ?>
-                            <div class="star-network-additional-page-content">
-                                <?php the_content(); ?>
+            <div class="star-network-content-wrapper-bg">
+                <div class="star-network-content middle-row">
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <div class="star-network-additional-page-title">
+                                <?php echo get_the_title(); ?>
                             </div>
-                        <?php endif; ?>
 
-                        <div>
-                            Speaking on a Star Network panel is a great way to educate our attendees and position
-                            yourself as the leader in a field or industry.
-                            If you are interested in participating in one of our panels please complete the form below
-                            and note which event you are interested in participating in.
-                        </div>
+                            <div class="star-network-additional-page-sublime">
+                                <?php echo get_the_excerpt(); ?>
+                            </div>
 
-
-                        <div class="form-wrapper">
-                            <div id="waitMe"></div>
-                            <form id="speak-form" method="post">
-                                <div class="name-input-wrapper">
-                                    <label for="name"></label>
-                                    <input type="text" name="name" required="" aria-required="true" id="name"
-                                           placeholder="First and Last"
-                                           class="speak-name-input"
-                                           value=""/>
+                            <?php if (get_the_content() !== ''): ?>
+                                <div class="star-network-additional-page-content">
+                                    <?php the_content(); ?>
                                 </div>
+                            <?php endif; ?>
 
-                                <div class="title-input-wrapper">
-                                    <label for="title"></label>
-                                    <input type="text" name="title" required="" aria-required="true" id="title"
-                                           placeholder="Title"
-                                           class="speak-title-input"
-                                           value=""/>
-                                </div>
+                            <div>
+                                Speaking on a Star Network panel is a great way to educate our attendees and position
+                                yourself as the leader in a field or industry.
+                                If you are interested in participating in one of our panels please complete the form below
+                                and note which event you are interested in participating in.
+                            </div>
 
-                                <div class="company-input-wrapper">
-                                    <label for="company"></label>
-                                    <input type="text" name="company" required="" aria-required="true" id="company"
-                                           placeholder="Company"
-                                           class="speak-company-input"
-                                           value=""/>
-                                </div>
 
-                                <div class="email-input-wrapper">
-                                    <label for="email"></label>
-                                    <input type="email" name="email" required="" aria-required="true" id="email"
-                                           placeholder="Email"
-                                           class="speak-email-input"
-                                           value=""/>
-                                </div>
-
-                                <div class="phone-input-wrapper">
-                                    <label for="phone"></label>
-                                    <input type="text" name="phone" required="" aria-required="true" id="phone"
-                                           placeholder="Phone"
-                                           class="speak-phone-input"
-                                           value=""/>
-                                </div>
-                                <div class="checkboxes">
-                                    <div class="checkboxes-title">
-                                        Topics:
+                            <div class="form-wrapper">
+                                <div id="waitMe"></div>
+                                <form id="speak-form" method="post">
+                                    <div class="name-input-wrapper">
+                                        <label for="name"></label>
+                                        <input type="text" name="name" required="" aria-required="true" id="name"
+                                               placeholder="First and Last"
+                                               class="speak-name-input"
+                                               value=""/>
                                     </div>
-                                    <div>
-                                        <label for="healthcare">
-                                            <input type="checkbox" name="cat[healthcare]"
-                                                   id="healthcare"
-                                                   value="Healthcare"/>
-                                            <span class="custom checkbox"></span>
-                                            Healthcase
 
-                                        </label>
+                                    <div class="title-input-wrapper">
+                                        <label for="title"></label>
+                                        <input type="text" name="title" required="" aria-required="true" id="title"
+                                               placeholder="Title"
+                                               class="speak-title-input"
+                                               value=""/>
                                     </div>
-                                    <div>
-                                        <label for="real-estate">
-                                            <input type="checkbox" name="cat[real_estate]" id="real-estate"
-                                                   value="Real Estate"/>
-                                            <span class="custom checkbox"></span>
-                                            Real Estate
 
-                                        </label>
+                                    <div class="company-input-wrapper">
+                                        <label for="company"></label>
+                                        <input type="text" name="company" required="" aria-required="true" id="company"
+                                               placeholder="Company"
+                                               class="speak-company-input"
+                                               value=""/>
                                     </div>
-                                    <div>
-                                        <label for="kids-education">
-                                            <input type="checkbox" name="cat[kids_education]" id="kids-education"
-                                                   value="Kids & Education"/>
-                                            <span class="custom checkbox"></span>
-                                            Kids & Education
 
-                                        </label>
+                                    <div class="email-input-wrapper">
+                                        <label for="email"></label>
+                                        <input type="email" name="email" required="" aria-required="true" id="email"
+                                               placeholder="Email"
+                                               class="speak-email-input"
+                                               value=""/>
                                     </div>
-                                    <div>
-                                        <label for="finance">
-                                            <input type="checkbox" name="cat[finance]" id="finance"
-                                                   value="Finance"/>
-                                            <span class="custom checkbox"></span>
-                                            Finance
 
-                                        </label>
+                                    <div class="phone-input-wrapper">
+                                        <label for="phone"></label>
+                                        <input type="text" name="phone" required="" aria-required="true" id="phone"
+                                               placeholder="Phone"
+                                               class="speak-phone-input"
+                                               value=""/>
                                     </div>
-                                </div>
+                                    <div class="checkboxes">
+                                        <div class="checkboxes-title">
+                                            Topics:
+                                        </div>
+                                        <div>
+                                            <label for="healthcare">
+                                                <input type="checkbox" name="cat[healthcare]"
+                                                       id="healthcare"
+                                                       value="Healthcare"/>
+                                                <span class="custom checkbox"></span>
+                                                Healthcase
 
-                                <div class="textarea">
-                                    <textarea placeholder="What event or topic is of interest to you?" name="comment" required="" aria-required="true"
-                                              id="comment"></textarea>
-                                </div>
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label for="real-estate">
+                                                <input type="checkbox" name="cat[real_estate]" id="real-estate"
+                                                       value="Real Estate"/>
+                                                <span class="custom checkbox"></span>
+                                                Real Estate
 
-                                <div class="data captcha">
-                                    <input type="text" id="defaultReal" name="defaultReal" placeholder="Enter Access Code" required="" aria-required="true"  maxlength="6"/>
-                                </div>
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label for="kids-education">
+                                                <input type="checkbox" name="cat[kids_education]" id="kids-education"
+                                                       value="Kids & Education"/>
+                                                <span class="custom checkbox"></span>
+                                                Kids & Education
 
-                                <div class="submit-news-letter-btn submit">
-                                    <input type="submit" value="submit" name="submit" class="news-letter-email-submit"/>
-                                </div>
-                            </form>
-                        </div>
-                    <?php endwhile; ?>
-                <?php endif; ?>
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label for="finance">
+                                                <input type="checkbox" name="cat[finance]" id="finance"
+                                                       value="Finance"/>
+                                                <span class="custom checkbox"></span>
+                                                Finance
+
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="textarea">
+                                        <textarea placeholder="What event or topic is of interest to you?" name="comment" required="" aria-required="true"
+                                                  id="comment"></textarea>
+                                    </div>
+
+                                    <div class="data captcha">
+                                        <input type="text" id="defaultReal" name="defaultReal" placeholder="Enter Access Code" required="" aria-required="true"  maxlength="6"/>
+                                    </div>
+
+                                    <div class="submit-news-letter-btn submit">
+                                        <input type="submit" value="submit" name="submit" class="news-letter-email-submit"/>
+                                    </div>
+                                </form>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
             </div>
 
         </div>
