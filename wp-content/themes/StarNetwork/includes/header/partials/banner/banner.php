7086d25c1cb3cac72e4b3c2bfba2deec49750931
Alexander Matvienko <amatvienko@archer-soft.com>
2016-09-09 19:09:40 +0300

    SCHNEP-592 Fixed leader board group

--- wp-content/themes/StarNetwork/includes/header/partials/banner/banner.php
+++ wp-content/themes/StarNetwork/includes/header/partials/banner/banner.php
@@ -5,17 +5,18 @@ if (count($explodeId) > 1) {
     $id = $explodeId[1];
     ?>
     <div class="banner">
+        <?php echo schneps_get_adrotate_($leaderboard_ad_rotate_id); ?>
         <?php
-        if ($explodeId[0] == 'Ad') {
-            echo adrotate_ad($id);
-        } else {
-            $group_ad = schneps_adrotate_group($id, true);
-            if (!empty($group_ad)) {
-                $matches = array();
-                preg_match('%<a(.*)/a>%', $group_ad, $matches);
-                echo $matches[0];
-            }
-        }
+//        if ($explodeId[0] == 'Ad') {
+//            echo adrotate_ad($id);
+//        } else {
+//            $group_ad = schneps_adrotate_group($id, true);
+//            if (!empty($group_ad)) {
+//                $matches = array();
+//                preg_match('%<a(.*)/a>%', $group_ad, $matches);
+//                echo $matches[0];
+//            }
+//        }
         ?>
     </div>
 <?php } ?>
